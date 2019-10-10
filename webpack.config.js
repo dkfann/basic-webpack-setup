68c4d5f6837ea28ef4e327d693a1e288306a2888
dkfann <danny.k.fann@gmail.com>
2019-10-10 00:52:23 -0700

    Add copy plugin import

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: [
